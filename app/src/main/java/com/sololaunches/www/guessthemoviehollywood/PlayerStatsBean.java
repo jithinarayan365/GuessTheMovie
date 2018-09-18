64c432a0d2b153deadf518c4aae33a0e88164626
jithin <jithinarayan365@gmail.com>
2018-09-18 20:40:07 +0530

    first

---
+++ app/src/main/java/com/sololaunches/www/guessthemoviehollywood/PlayerStatsBean.java
@@ -0,0 +1,49 @@
+package com.sololaunches.www.guessthemoviehollywood;
+
+/**
+ * Created by hp on 26-03-2018.
+ */
+
+public class PlayerStatsBean {
+
+    private String player;
+    private String coins;
+    private String points;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+
+    public String getCoins() {
+        return coins;
+    }
+
+    public void setCoins(String coins) {
+        this.coins = coins;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/sololaunches/www/guessthemoviehollywood/WWEEditText.java
@@ -0,0 +1,44 @@
+package com.sololaunches.www.guessthemoviehollywood;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class WWEEditText extends AppCompatEditText {
+
+
+
+    public WWEEditText(Context context) {
+        super(context);
+    }
+
+    public WWEEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WWEEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean dispatchKeyEventPreIme(KeyEvent event) {
+        if(event.getKeyCode() == KeyEvent.KEYCODE_BACK){
+            clearFocus();
+        }
+        return super.dispatchKeyEventPreIme(event);
+    }
+    @Override
+    public boolean onKeyPreIme(int keyCode, KeyEvent event)
+    {
+        if(keyCode == KeyEvent.KEYCODE_BACK)
+        {
+            clearFocus();
+        }
+        return super.onKeyPreIme(keyCode, event);
+    }
+
+
+}
